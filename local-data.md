daff1dd34ed71db7b3388976bd0936471afbe30e
Asif <asif.h@allshorestaffing.com>
2024-08-24 00:09:06 +0500

    Initial setup

---
+++ local-data.md
@@ -0,0 +1,1 @@
+https://images.unsplash.com/photo-1719937206590-6cb10b099e0f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'apn-site';
-}
+  title = 'apn-site'; 
+  
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+      const navbar = document.getElementById('mainNav');
+      if (window.pageYOffset > 80) {
+        navbar?.classList.add('navbar-scrolled');
+      } else {
+        navbar?.classList.remove('navbar-scrolled');
+      }
+    }
+  }
