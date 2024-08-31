eb61e33bc33f65eeff40740c93c14bbd8db3a0c1
Muhammad Asif <mhdaxif@gmail.com>
2024-08-31 18:35:13 +0500

    Add company data

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ServicesComponent } from "../services/services.component";
 import { RouterModule } from '@angular/router';
 import { SliderComponent } from '../slider/slider.component';
+import { HeroComponent } from '../shared/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliderComponent, ServicesComponent, RouterModule],
+  imports: [SliderComponent, HeroComponent, ServicesComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
