a1807c445b861975fda6d7bf38504a10c3c97b99
Muhammad Asif <mhdaxif@gmail.com>
2024-08-31 18:53:43 +0500

    Add about

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,18 @@ import { ServicesComponent } from "../services/services.component";
 import { RouterModule } from '@angular/router';
 import { SliderComponent } from '../slider/slider.component';
 import { HeroComponent } from '../shared/hero/hero.component';
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliderComponent, HeroComponent, ServicesComponent, RouterModule],
+  imports: [
+    SliderComponent, 
+    HeroComponent, 
+    ServicesComponent, 
+    AboutComponent,
+    RouterModule, 
+    AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
